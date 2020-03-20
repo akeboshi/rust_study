4b8286b6852efe38e37f57c9272b479b2de1ace9
akeboshi <isao-aruga@bm-sms.co.jp>
2020-03-20 22:04:06 +0900

    add

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
+    let x = return5();
+    println!("{}", x);
     println!("Hello, world!");
+}
+
+fn return5() -> i64{
+    return 5;
+}
+
+#[test]
+fn return5_test() {
+    assert_eq!(4, return5())
 }
