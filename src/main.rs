d34ebefd55c243d9a8dcdc34c35f90b389528dd1
akeboshi <isao-aruga@bm-sms.co.jp>
2020-03-27 23:46:43 +0900

    add 4-2

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate rust_study;
 
 pub mod r1;
 pub mod r4_1;
+pub mod r4_2;
 
 fn main() {
     let x = rust_study::return5();
---
+++ src/r4_2.rs
@@ -0,0 +1,23 @@
+fn return7() -> i32 {
+    7
+}
+
+fn ver2_diverges() -> ! {
+    panic!("panic だよ");
+}
+
+#[test]
+fn immutable_test() {
+    let f: fn() -> i32 = return7;
+    assert_eq!(f(), 7)
+}
+
+use std::panic;
+
+#[test]
+fn diverges_test() {
+    let result = panic::catch_unwind(|| {
+        ver2_diverges();
+    });
+    assert!(result.is_err());
+}
